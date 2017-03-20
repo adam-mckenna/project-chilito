bb6fc56522e663a99952d6b453d2f3bd8d3015c2
Adam McKenna <adam-mckenna@outlook.com>
2017-03-20 19:49:10 +0000

    Added a few tests

--- app/Api/Controllers/Auth/AuthenticationController.php
+++ app/Api/Controllers/Auth/AuthenticationController.php
@@ -6,14 +6,12 @@ use App\Api\Models\User;
 use App\Api\Transformers\UserTransformer;
 use Dingo\Api\Contract\Http\Request;
 use Dingo\Api\Routing\Helpers;
-use Illuminate\Auth\Events\Failed;
 use Illuminate\Auth\Events\Login;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
-use Tymon\JWTAuth\Exceptions\JWTException;
 use Tymon\JWTAuth\JWTAuth;
 
 class AuthenticationController extends Controller
@@ -30,32 +28,12 @@ class AuthenticationController extends Controller
         $this->auth = $auth;
     }
 
-    public function authenticate()
-    {
-        dd('eyup');
-//        $credentials = $request->only('email', 'password');
-//
-//        try {
-//            // attempt to verify the credentials and create a token for the user
-//            if (! $token = JWTAuth->attempt($credentials)) {
-//                return response()->json(['error' => 'invalid_credentials'], 401);
-//            }
-//        } catch (JWTException $e) {
-//            // something went wrong whilst attempting to encode the token
-//            return response()->json(['error' => 'could_not_create_token'], 500);
-//        }
-//
-//        // all good so return the token
-//        return response()->json(compact('token'));
-    }
-
     public function login(Request $request)
     {
         $credentials = $request->only('email', 'password');
         $user = User::where('email', $credentials['email'])->where('active', 1)->first();
-        if (!$user || !$token = $this->auth->attempt($credentials)) {
+        if (!$user || !$token = $this->auth->attempt($credentials))
             throw new NotFoundHttpException;
-        }
 
         $remember = true;
         event(new Login(auth()->user(), $remember));
@@ -65,7 +43,7 @@ class AuthenticationController extends Controller
 
     public function logout()
     {
-        dd('eyp');
+         dd('eyp');
         $this->authService->logout();
         return $this->response->noContent();
     }
---
+++ tests/Unit/UserRepositoryTest.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Api\Models\User;
+use App\Api\Repositories\UserRepository;
+use Faker\Factory as Faker;
+use Illuminate\Database\QueryException;
+use Tests\TestCase;
+
+class UserRepositoryTest extends TestCase
+{
+    private $faker;
+    private $userRepo;
+
+    public function setUp()
+    {
+        parent::setUp();
+        $this->faker = Faker::create();
+        $this->userRepo = new UserRepository();
+    }
+
+    /**
+     * Test that a user can be created
+     *
+     * @return void
+     */
+    public function testUserCanBeCreatedFromValidDetails()
+    {
+        $id = count(User::all()) + 2;
+        $user = $this->userRepo->create($id, [
+            'name' => $this->faker->name,
+            'email' => $this->faker->email,
+            'password' => 'password',
+            'active' => 1
+        ]);
+        $this->assertInstanceOf(User::class, $user);
+    }
+
+    /**
+     * Test that a user with invalid credentials be created
+     *
+     * @return void
+     */
+    public function testUserCannotBeCreatedWithInvalidCredentials()
+    {
+
+        $this->expectException(QueryException::class);
+        $id = count(User::all()) + 2;
+        $user = $this->userRepo->create($id, [
+            'name' => '',
+            'email' => 'no email',
+            'password' => 'password',
+            'active' => 1
+        ]);
+    }
+}
