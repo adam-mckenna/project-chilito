f389080db12472d53f1395626b52adc0e3c7cdf1
Adam McKenna <adam-mckenna@outlook.com>
2017-04-14 11:11:42 +0100

    Migrations - small changes + added avatars table

--- database/migrations/2017_04_13_215630_create_avatars_table.php
+++ database/migrations/2017_04_13_215630_create_avatars_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateAvatarsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,11 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('avatars', function (Blueprint $table) {
             $table->timestamps();
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
+            $table->integer('user_id');
+            $table->string('filename');
         });
     }
 
@@ -30,6 +28,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::drop('avatars');
     }
 }
